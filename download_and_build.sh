983c6ffc72f78d471221bfa22fadb576ca141047
novec.tech <90937877+novec-tech@users.noreply.github.com>
2022-04-16 11:41:22 +0200

    Update download_and_build.sh

--- download_and_build.sh
+++ download_and_build.sh
@@ -1,6 +1,5 @@
-!#bin/bash
-
 cd /home/ipxe/src/
+apt install curl -y
 curl https://github.com/novec-tech/boot.novec.tech/blob/main/custom.ipxe -output custom.ipxe
 rm -rf bin
 make bin/ipxe.iso EMBED=custom.ipxe
